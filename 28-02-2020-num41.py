92f78a95f3f495f701fdd97365b56beb13e6067f
Vincent Josse <vincent.josse@telecom-paris.fr>
2020-02-28 19:04:38 +0100

    quick commit

---
+++ 28-02-2020-num41.py
@@ -0,0 +1,11 @@
+# Given a list of pairs of airports, find the global itinary. It must go
+# through every element in the list. If multiple itinaries exits, return the
+# first one in lexicographic order. If none, return null.
+
+test1 = [('A', 'B'), ('A', 'C'), ('B', 'C'), ('C', 'A')]
+
+def find_itinary(l):
+
+
+
+print(find_itinary(test1))
